da4e41451d1d0d28b12f9dbb957dfeb945be1312
Elias Michel Oziolor <oziolor@gmail.com>
2018-10-12 14:20:35 -0700

    trimmed and aligned reads, running stats on them with samtools flagstat

--- scripts/trimalign/trimalign.sh
+++ scripts/trimalign/trimalign.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 
 #SBATCH -J combo_trimalign
-#SBATCH --array=1-10
+#SBATCH --array=1-1275
 #SBATCH -e combo_trimalign%A-%a.o
 #SBATCH -o combo_trimalign%A-%a.o
 #SBATCH -N 1
@@ -47,13 +47,13 @@ fq2=$my_dir/AWPH*$num/*2.fq.gz
 my_bwa=/home/eoziolor/program/bwa-0.7.17/bwa
 my_sbl=/home/eoziolor/program/samblaster/samblaster
 my_sam=/home/eoziolor/program/samtools-1.9/samtools
-my_out=/home/eoziolor/phpopg/data/align/
+my_out=/home/eoziolor/phpopg/data/align
 my_gen=/home/eoziolor/phgenome/data/genome/phgenome_masked.fasta
 my_list=/home/eoziolor/phpopg/data/list/zeros_samples.tsv
 
 #others
 pop=$(cat $my_list | grep $sample | cut -f 2)
-rg=$(echo \@RG\\tID:$sample\\tPL:Illumina\\tPU:x\\tLB:combined\\tSM:$sample.$pop)
+rg=$(echo \@RG\\tID:$sample\\tPL:Illumina\\tPU:x\\tLB:pherring\\tSM:$sample.$pop)
 outroot=$sample\_$pop
 
 #Code
@@ -61,6 +61,6 @@ paste <(zcat $fq1 | paste - - - -) \
       <(zcat $fq2 | paste - - - -) |\
 tr '\t' '\n' |\
 cutadapt -j 8 --interleaved -a CTGTCTCTTATA -A CTGTCTCTTATA -u 10 -U 10 -q 30 --trim-n --minimum-length 36 - |\
-$my_bwa mem $my_gen -p -R $rg -t 2 - |\
+$my_bwa mem $my_gen -p -R $rg -t 8 - |\
 $my_sam view -S -h -u - | \
-$my_sam sort -T $my_out/$outroot > $my_out/$outroot
+$my_sam sort -T $my_out/$outroot > $my_out/$outroot\.bam
